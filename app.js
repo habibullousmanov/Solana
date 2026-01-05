1dfd61f3ff210e173318d15486204a1cbf251d87
habibullousmanov <habibullousmanov82@gmail.com>
2026-01-05 07:04:41 +0500

    Solana

---
+++ app.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector(".btn.primary");
+
+btn.addEventListener("click", () => {
+ window.location.href = "create.html";
+});
+const community = document.querySelector(".community");
+
+window.addEventListener("scroll", () => {
+  const rect = community.getBoundingClientRect();
+  if (rect.top < window.innerHeight - 100) {
+    community.classList.add("show");
+  }
+});
